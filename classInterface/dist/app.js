0048d49da2de461531e80f5631bbf839394e64c2
jingjielim <jingjie3000@gmail.com>
2021-04-03 01:07:42 +0800

    Creating a First Class

---
+++ classInterface/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var Department = /** @class */ (function () {
+    function Department(name) {
+        this.name = name;
+    }
+    return Department;
+}());
+var accounting = new Department("accounting");
+console.log(accounting);
+//# sourceMappingURL=app.js.map
---
+++ classInterface/src/app.ts
@@ -0,0 +1,12 @@
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+let accounting = new Department("accounting")
+
+console.log(accounting);
+
