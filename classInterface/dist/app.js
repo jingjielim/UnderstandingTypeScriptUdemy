4bc856a665d70008415c8131dbafd1afe530d133
jingjielim <jingjie3000@gmail.com>
2021-04-03 12:45:20 +0800

    public private properties

--- classInterface/dist/app.js
+++ classInterface/dist/app.js
@@ -1,10 +1,29 @@
 "use strict";
 var Department = /** @class */ (function () {
     function Department(name) {
+        this.employees = [];
         this.name = name;
     }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("accounting");
+accounting.addEmployee("jingjie");
+accounting.addEmployee("Max");
+// Unable to access employees property of accounting anymore
+// accounting.employees[2] = "Anna";
 console.log(accounting);
+accounting.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "jingjie", describe: accounting.describe };
+// accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- classInterface/src/app.ts
+++ classInterface/src/app.ts
@@ -1,12 +1,36 @@
 class Department {
-  name: string;
+  private name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-let accounting = new Department("accounting")
+let accounting = new Department("accounting");
+accounting.addEmployee("jingjie");
+accounting.addEmployee("Max");
+// Unable to access employees property of accounting anymore
+// accounting.employees[2] = "Anna";
 
 console.log(accounting);
 
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "jingjie", describe: accounting.describe };
+
+// accountingCopy.describe();
