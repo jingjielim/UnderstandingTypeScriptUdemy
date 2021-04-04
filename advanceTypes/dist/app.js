1cdf15f3682d08a11e697a2670053c17d56bab3a
jingjielim <jingjie3000@gmail.com>
2021-04-05 00:19:11 +0800

    Using Type Guards

--- advanceTypes/dist/app.js
+++ advanceTypes/dist/app.js
@@ -5,3 +5,49 @@ var e1 = {
     startDate: new Date(),
 };
 console.log(e1);
+// Type guarding using typeof
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log("name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date is: " + emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving...");
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving a truck....");
+    };
+    Truck.prototype.loadCargo = function (amt) {
+        console.log("Loading cargo... " + amt);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(10);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- advanceTypes/src/app.ts
+++ advanceTypes/src/app.ts
@@ -24,3 +24,56 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
 console.log(e1);
+
+// Type guarding using typeof
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+// Type guarding in TS
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date is: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck....");
+  }
+  loadCargo(amt: number) {
+    console.log("Loading cargo... " + amt);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(10);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
