047e601b44f55d10ad7c8df0758a013e41b6a0ad
jingjielim <jingjie3000@gmail.com>
2021-04-04 22:54:29 +0800

    Extending interfaces

--- classInterface/dist/interfaces.js
+++ classInterface/dist/interfaces.js
@@ -1,4 +1,9 @@
 "use strict";
+var add;
+add = function (num1, num2) {
+    return num1 + num2;
+};
+// A class can only extend from one other class but can implement multiple interfaces
 var Person = /** @class */ (function () {
     function Person(name) {
         this.name = name;
@@ -12,4 +17,6 @@ var Person = /** @class */ (function () {
 var user1;
 user1 = new Person("Jing Jie");
 user1.greet("Hi there, I am");
+// Unable to reassign name as it is a readonly property
+// user1.name = "Hazel";
 //# sourceMappingURL=interfaces.js.map
--- classInterface/src/interfaces.ts
+++ classInterface/src/interfaces.ts
@@ -1,22 +1,37 @@
-interface Greetable {
-  name: string;
+// Interface can also be used as a function type if we do not declare a method name.
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+interface Named {
+  readonly name: string;
+}
+
+// Interface can extend from one or more other interfaces
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
-class Person implements Greetable{
-    age = 30;
+// A class can only extend from one other class but can implement multiple interfaces
+class Person implements Greetable {
+  age = 30;
 
-    constructor(public name: string){
+  constructor(public name: string) {}
 
-    }
-
-    greet(phrase: string) {
-        console.log(phrase + " " + this.name);
-        
-    }
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
 }
 let user1: Greetable;
 
-user1 = new Person("Jing Jie")
+user1 = new Person("Jing Jie");
 
 user1.greet("Hi there, I am");
+// Unable to reassign name as it is a readonly property
+// user1.name = "Hazel";
